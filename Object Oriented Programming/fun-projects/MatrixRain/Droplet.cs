f64ceb0aae7a7bf27f75c97cb9004ebf8cc12bf4
dbEthan <73438080+dbEthan@users.noreply.github.com>
2021-03-14 22:42:05 +0100

    Added a fun little Matrix Rain - WIP

---
+++ Object Oriented Programming/fun-projects/MatrixRain/Droplet.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixRain
+{
+    class Droplet
+    {
+        Random r = new Random();
+        const int Y_START_POS = 0;
+        public char Char { get; set; }
+        public int X { get; private set; }
+        public int Y { get; private set; } = Y_START_POS;
+
+        public void SetStartPos()
+        {
+            X = r.Next(1, Console.WindowWidth);
+        }
+        public void CharPosUpdate()
+        {
+            int chance = r.Next(1, 5);
+            if (Y == 0)
+                Y++;
+            if (chance < 3)
+                Y++;
+        }
+
+        public void DrawOnScreen()
+        {
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(X, Y);
+            GiveColor();
+            Console.WriteLine(CharRandomnizer());
+            Console.ResetColor();
+        }
+
+        public void GiveColor()
+        {
+            int chance = r.Next(1, 5);
+            if (chance == 1)
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+            else
+                Console.ForegroundColor = ConsoleColor.Green;
+        }
+
+        public char CharRandomnizer()
+        {
+            Char = (char)r.Next('A', 'Z');
+            return Char;
+        }
+    }
+}
---
+++ Object Oriented Programming/fun-projects/MatrixRain/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MatrixRain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Droplet[] dropletArr = new Droplet[15];
+
+            for (int i = 0; i < dropletArr.Length; i++)
+            {
+                dropletArr[i] = new Droplet();
+
+                dropletArr[i].SetStartPos();
+            }
+
+            for (int j = 0; j < 100; j++)
+            {
+                for (int i = 0; i < dropletArr.Length; i++)
+                {
+                    dropletArr[i].CharPosUpdate();
+                    dropletArr[i].DrawOnScreen();
+                    System.Threading.Thread.Sleep(20);
+                }
+                
+            }
+        }
+    }
+}
